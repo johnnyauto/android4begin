70239651b76befe96606801618caae105db4e8c0
hoang.nguyen <hoang.nguyen@titancorpvn.com>
2018-09-19 17:29:40 +0700

    feat(app): Add lifecycle log and restore the Activity instance

--- TwoActivities/app/src/main/java/com/example/hoangnguyen/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/hoangnguyen/twoactivities/SecondActivity.java
@@ -3,12 +3,14 @@ package com.example.hoangnguyen.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    public static final String LOG_TAG = SecondActivity.class.getSimpleName();
     public static final String EXTRA_REPLY =
             "com.example.hoangnguyen.twoactivities.extra.REPLY";
     private EditText mReply;
@@ -31,6 +33,7 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
