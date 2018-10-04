058fa3a9504fc873fbe9a880681827bf810102f4
hoang.nguyen <hoang.nguyen@titancorpvn.com>
2018-10-04 16:36:21 +0700

    feat(DiaLogForAlert): Using a picker

--- DialogForAlert/app/src/main/java/com/example/hoangnguyen/dialogforalert/MainActivity.java
+++ DialogForAlert/app/src/main/java/com/example/hoangnguyen/dialogforalert/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hoangnguyen.dialogforalert;
 
 import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -32,5 +33,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         myAlertBuilder.show();
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.datepicker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+        Toast.makeText(this, getString(R.string.date) + dateMessage, Toast.LENGTH_SHORT).show();
     }
 }
